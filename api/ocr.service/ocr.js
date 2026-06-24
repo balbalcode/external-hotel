1bf4910182c76c27a142db50a3286aeb48583efa
balbalcode <iqbalmaulana027@gmail.com>
2026-06-24 09:28:10 +0700

    fix: back to read data

--- api/ocr.service/ocr.js
+++ api/ocr.service/ocr.js
@@ -25,7 +25,7 @@ async function read(data) {
   let axios = window.$nuxt.$axios;
   axios.defaults.baseURL = process.env.OCR_URL;
   return axios
-    .post(`${process.env.OCR_URL}/mock`, data, { timeout: 25500 })
+    .post(`${process.env.OCR_URL}/read`, data, { timeout: 25500 })
     .then((data) => {
       axios.defaults.baseURL = process.env.baseUrl;
       if (data.status === 200) return data.data;
